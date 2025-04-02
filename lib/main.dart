4a125ecc53ad77252456ec338c8cd927870b9a7c
TwinklerG <231830131@smail.nju.edu.cn>
2025-04-02 19:54:25 +0800

    support enter key to confirm

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -93,73 +94,89 @@ class _LoginFormState extends State<LoginForm> {
     });
   }
 
+  void connect(AppState appState) async {
+    setState(() {
+      logs = "尝试连接中";
+    });
+    var url = Uri.parse("https://p.nju.edu.cn/api/portal/v1/login");
+    var headers = {
+      "Content-Type": "application/json",
+    };
+    var body = jsonEncode({
+      "username": usernameController.text,
+      "password": passwordController.text,
+    });
+    http.post(url, headers: headers, body: body).then((result) {
+      var body = json.decode(result.body);
+      if (body["reply_code"] == 0) {
+        appState.setIsLogin(true);
+        if (kDebugMode) {
+          print("登录成功✌️");
+        }
+      } else if (body["reply_code"] == 500) {
+        if (kDebugMode) {
+          print("登录失败");
+        }
+      }
+    }).catchError((error) {
+      if (kDebugMode) {
+        print(error);
+      }
+      setState(() {
+        logs = "登录失败。未连接NJU-WLAN或服务器内部错误";
+      });
+    });
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString("username", usernameController.text);
+    await prefs.setString("password", passwordController.text);
+  }
+
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<AppState>();
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        TextField(
-          controller: usernameController,
-          decoration: InputDecoration(
-              border: OutlineInputBorder(), labelText: "username"),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 4),
-        ),
-        TextField(
-          controller: passwordController,
-          obscureText: true,
-          decoration: InputDecoration(
-              border: OutlineInputBorder(), labelText: "password"),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 4),
-        ),
-        ElevatedButton(
-            onPressed: () async {
-              setState(() {
-                logs = "尝试连接中";
-              });
-              var url = Uri.parse("https://p.nju.edu.cn/api/portal/v1/login");
-              var headers = {
-                "Content-Type": "application/json",
-              };
-              var body = jsonEncode({
-                "username": usernameController.text,
-                "password": passwordController.text,
-              });
-              http.post(url, headers: headers, body: body).then((result) {
-                var body = json.decode(result.body);
-                if (body["reply_code"] == 0) {
-                  appState.setIsLogin(true);
-                  if (kDebugMode) {
-                    print("登录成功✌️");
-                  }
-                } else if (body["reply_code"] == 500) {
-                  if (kDebugMode) {
-                    print("登录失败");
-                  }
-                }
-              }).catchError((error) {
-                if (kDebugMode) {
-                  print(error);
-                }
-                setState(() {
-                  logs = "登录失败。未连接NJU-WLAN或服务器内部错误";
-                });
-              });
-              final SharedPreferences prefs =
-                  await SharedPreferences.getInstance();
-              await prefs.setString("username", usernameController.text);
-              await prefs.setString("password", passwordController.text);
-            },
-            child: const Text("登录")),
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 4),
-        ),
-        Text(logs),
-      ],
+    return KeyboardListener(
+      focusNode: FocusNode()..requestFocus(),
+      onKeyEvent: (KeyEvent event) {
+        if (event.logicalKey == LogicalKeyboardKey.enter) {
+          if (event is KeyDownEvent && !appState._pressedKeys.contains(event.logicalKey)) {
+            connect(appState);
+            appState.addPressedKeys(LogicalKeyboardKey.enter);
+          } else if (event is KeyUpEvent) {
+            appState.removePressedKeys(LogicalKeyboardKey.enter);
+          }
+        }
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextField(
+            controller: usernameController,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(), labelText: "username"),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4),
+          ),
+          TextField(
+            controller: passwordController,
+            obscureText: true,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(), labelText: "password"),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4),
+          ),
+          ElevatedButton(
+              onPressed: () async {
+                connect(appState);
+              },
+              child: const Text("登录")),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 4),
+          ),
+          Text(logs),
+        ],
+      ),
     );
   }
 }
@@ -174,55 +191,72 @@ class SuccessForm extends StatefulWidget {
 class _SuccessFormState extends State<SuccessForm> {
   String logs = "";
 
+  void disconnect(AppState appState) async {
+    setState(() {
+      logs = "尝试登出中";
+    });
+    var url = Uri.parse("https://p.nju.edu.cn/api/portal/v1/logout");
+    var headers = {
+      "Content-Type": "application/json",
+    };
+    var body = jsonEncode({});
+    http.post(url, headers: headers, body: body).then((result) {
+      var body = json.decode(result.body);
+      if (body["reply_code"] == 0) {
+        appState.setIsLogin(false);
+        if (kDebugMode) {
+          print("登出成功✌️");
+        }
+      } else if (body["reply_code"] == 500) {
+        if (kDebugMode) {
+          print("登出失败");
+        }
+      }
+    }).catchError((error) {
+      setState(() {
+        if (kDebugMode) {
+          print(error);
+        }
+        setState(() {
+          logs = "登出失败。未连接NJU-WLAN或服务器内部错误";
+        });
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<AppState>();
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text("登录成功✌️"),
-          Padding(padding: EdgeInsets.symmetric(vertical: 4)),
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                logs = "尝试登出中";
-              });
-              var url = Uri.parse("https://p.nju.edu.cn/api/portal/v1/logout");
-              var headers = {
-                "Content-Type": "application/json",
-              };
-              var body = jsonEncode({});
-              http.post(url, headers: headers, body: body).then((result) {
-                var body = json.decode(result.body);
-                if (body["reply_code"] == 0) {
-                  appState.setIsLogin(false);
-                  if (kDebugMode) {
-                    print("登出成功✌️");
-                  }
-                } else if (body["reply_code"] == 500) {
-                  if (kDebugMode) {
-                    print("登出失败");
-                  }
-                }
-              }).catchError((error) {
-                setState(() {
-                  if (kDebugMode) {
-                    print(error);
-                  }
-                  setState(() {
-                    logs = "登出失败。未连接NJU-WLAN或服务器内部错误";
-                  });
-                });
-              });
-            },
-            child: const Text("登出"),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 4),
-          ),
-          Text(logs),
-        ],
+      child: KeyboardListener(
+        focusNode: FocusNode()..requestFocus(),
+        onKeyEvent: (KeyEvent event) {
+          if (event.logicalKey == LogicalKeyboardKey.enter) {
+            if (event is KeyDownEvent && !appState._pressedKeys.contains(event.logicalKey)) {
+              disconnect(appState);
+              appState.addPressedKeys(LogicalKeyboardKey.enter);
+            } else if (event is KeyUpEvent) {
+              appState.removePressedKeys(LogicalKeyboardKey.enter);
+            }
+          }
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("登录成功✌️"),
+            Padding(padding: EdgeInsets.symmetric(vertical: 4)),
+            ElevatedButton(
+              onPressed: () {
+                disconnect(appState);
+              },
+              child: const Text("登出"),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 4),
+            ),
+            Text(logs),
+          ],
+        ),
       ),
     );
   }
@@ -232,6 +266,16 @@ class AppState extends ChangeNotifier {
   var isLogin = false;
   void setIsLogin(bool isLogin) {
     this.isLogin = isLogin;
+    notifyListeners();
+  }
+
+  final Set<LogicalKeyboardKey> _pressedKeys = {};
+  void addPressedKeys(LogicalKeyboardKey key) {
+    _pressedKeys.add(key);
+    notifyListeners();
+  }
+  void removePressedKeys(LogicalKeyboardKey key) {
+    _pressedKeys.remove(key);
     notifyListeners();
   }
 }
